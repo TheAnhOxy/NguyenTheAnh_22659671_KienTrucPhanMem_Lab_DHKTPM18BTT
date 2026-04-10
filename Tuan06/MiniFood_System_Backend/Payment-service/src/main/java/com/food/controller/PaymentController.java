f22d4c9a870a3b5377151488a893f39b0c2da7a5
Thế Anh Oxy <139890192+TheAnhOxy@users.noreply.github.com>
2026-04-10 22:41:35 +0700

    fix: update appilication url posgre

--- Tuan06/MiniFood_System_Backend/Payment-service/src/main/java/com/food/controller/PaymentController.java
+++ Tuan06/MiniFood_System_Backend/Payment-service/src/main/java/com/food/controller/PaymentController.java
@@ -1,17 +1,34 @@
 package com.food.controller;
 
-
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
+import com.food.dto.request.PaymentRequest;
+import com.food.dto.response.ApiResponse;
+import com.food.dto.response.PaymentResponse;
+import com.food.service.PaymentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@Slf4j
+@RequestMapping("/payments")
+@RequiredArgsConstructor
 public class PaymentController {
 
-    @GetMapping("/payment")
-    public String getHello(){
-        log.info("wwao wao");
-        return  "Hello Payment";
+    private final PaymentService paymentService;
+
+    @PostMapping
+    public ResponseEntity<ApiResponse> processPayment(@RequestBody PaymentRequest request) {
+        PaymentResponse response = paymentService.processPayment(request);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(
+                ApiResponse.builder()
+                        .status(HttpStatus.CREATED.value())
+                        .message("Xử lý thanh toán thành công")
+                        .data(response)
+                        .build()
+        );
     }
 }

41119e42b3fcecde59e7d4834ff0987add81f85d
Thế Anh Oxy <139890192+TheAnhOxy@users.noreply.github.com>
2026-04-10 17:23:59 +0700

    feat: init project service-based architecture

---
+++ Tuan06/MiniFood_System_Backend/Payment-service/src/main/java/com/food/controller/PaymentController.java
@@ -0,0 +1,17 @@
+package com.food.controller;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class PaymentController {
+
+    @GetMapping("/payment")
+    public String getHello(){
+        log.info("wwao wao");
+        return  "Hello Payment";
+    }
+}

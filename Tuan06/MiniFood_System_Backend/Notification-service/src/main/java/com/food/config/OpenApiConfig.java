dda206bbd3f6f9706a47738e84f1f02866ca6c5f
Thế Anh Oxy <139890192+TheAnhOxy@users.noreply.github.com>
2026-04-10 22:44:49 +0700

    feat : create notification and send message payment

--- Tuan06/MiniFood_System_Backend/Notification-service/src/main/java/com/food/config/OpenApiConfig.java
+++ Tuan06/MiniFood_System_Backend/Notification-service/src/main/java/com/food/config/OpenApiConfig.java
@@ -1,4 +1,4 @@
-package com.food.notificationservice.config;
+package com.food.config;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
---
+++ Tuan06/MiniFood_System_Backend/Notification-service/src/main/java/com/food/controller/NotificationController.java
@@ -0,0 +1,22 @@
+package com.food.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/notifications")
+public class NotificationController {
+
+    @PostMapping("/send")
+    public ResponseEntity<Void> sendNotification(@RequestBody Map<String, String> payload) {
+        String message = payload.get("message");
+
+        // In ra màn hình để chứng minh Microservices đã thông luồng
+        System.out.println("==============================================");
+        System.out.println("[THÔNG BÁO MỚI]: " + message);
+        System.out.println("==============================================");
+
+        return ResponseEntity.ok().build();
+    }
+}
